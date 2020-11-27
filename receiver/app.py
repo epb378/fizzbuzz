f9326bc857d2363ad35da1d47d2772f253270cc6
Ned Booker <63317915+epb378@users.noreply.github.com>
2020-11-27 06:06:14 -0500

    Add files via upload

--- receiver/app.py
+++ receiver/app.py
@@ -3,7 +3,7 @@ from time import time, sleep
 
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://user:pwd@10.5.0.3:27017/mydatabase")
+myclient = pymongo.MongoClient("mongodb://10.5.0.3:27017/mydatabase")
 mydb = myclient["mydatabase"]
 mycol = mydb["messages"]
 
@@ -12,7 +12,7 @@ number_list=[]
 @app.route('/', methods=['POST'])
 def hello(mycol=mycol):
     incoming_dict=request.form
-    print(incoming_dict)
+    #print(incoming_dict)
     received_time=time()*1000
     number_list.append(incoming_dict)
     
@@ -23,8 +23,7 @@ def hello(mycol=mycol):
 	} 
     x = mycol.insert_one(mydict)
 
-    #print(request.form) #should display a bunch of numbers
-    return my_dict
+    return 'number '+ incoming_dict['number'] +'sent_time' + incoming_dict['time'] + 'received_time' + str(received_time) + 'latency' + str(received_time-float(incoming_dict['time']))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
